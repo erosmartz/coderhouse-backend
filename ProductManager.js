0c56889fddc4624d102681346d4ed4df8dc3090c
Eros <107828862+erosmartz@users.noreply.github.com>
2023-06-29 18:30:08 -0300

    Update: Desafio 2
    updated script
    Contiene el proceso de testing incluido.

--- ProductManager.js
+++ ProductManager.js
@@ -1,43 +1,138 @@
 ///////////////////////////////////////
-// CLASE ProductManager
+// CLASE ProductManager - Desafio 2
 ///////////////////////////////////////
 
 
+const fs = require('fs');
+
+
+
+
 class ProductManager {
-  constructor() {
+  constructor(path) {
+
+    this.path = path;
     this.products = [];
     this.productId = 1;
+
   }
 
   addProduct(product) {
-    // Validar que todos los campos sean obligatorios
+
+
+    // Validar campos obligatorios
     if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
       console.log("Todos los campos son obligatorios.");
       return;
+      
     }
 
-    // Validar que no se repita el campo "code"
+    // Leer archivo para recibir los productos existentes
+    this.readProductsFromFile();
+
+
+
+    // Validar campo "code", que no se repita
     const existingProduct = this.products.find(p => p.code === product.code);
     if (existingProduct) {
-      console.log(`Ya existe un producto con el código ${product.code}.`);
+      console.log(`Ya existe un producto con el código de ID ${product.code}.`);
       return;
     }
 
-    // Agregar el producto al arreglo con un id autoincrementable
+    // Asignar un id autoincrementable y agregar el producto al arreglo
     product.id = this.productId++;
     this.products.push(product);
+
+    // Guardar los productos en el file
+    this.saveProductsToFile();
   }
 
+
+
   getProducts() {
+    // Leer archivo y obtener todos los productos
+    this.readProductsFromFile();
     return this.products;
   }
 
+
+
+
   getProductById(id) {
+    // Leer archivo y obtener productos en id
+    this.readProductsFromFile();
+
     const product = this.products.find(p => p.id === id);
     if (!product) {
-      console.log("Not found.");
+      console.log("Producto no encontrado.");
     }
     return product;
+  }
+
+
+
+
+  updateProduct(id, updatedProduct) {
+
+
+    // Leer el archivo y obtener los productos existentes
+    this.readProductsFromFile();
+
+
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex === -1) {
+      console.log("Producto no encontrado.");
+      return;
+    }
+
+
+    // Actualizar el producto en el arreglo
+    this.products[productIndex] = { ...this.products[productIndex], ...updatedProduct };
+
+
+    // Guardar los productos actualizados en el archivo
+    this.saveProductsToFile();
+  }
+
+
+
+
+
+  deleteProduct(id) {
+    // Leer el archivo y obtener los productos existentes
+    this.readProductsFromFile();
+
+    const productIndex = this.products.findIndex(p => p.id === id);
+    if (productIndex === -1) {
+      console.log("Producto no encontrado.");
+      return;
+    }
+
+    // Eliminar el producto del arreglo
+    this.products.splice(productIndex, 1);
+
+
+    // Guardar los productos actualizados en el archivo
+    this.saveProductsToFile();
+
+  }
+
+
+
+
+  readProductsFromFile() {
+
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      this.products = JSON.parse(data);
+    } catch (error) {
+      // Si ocurre un error al leer archivo, quiere decir que no hay productos todavia
+      this.products = [];
+    }
+  }
+
+  saveProductsToFile() {
+    fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'utf-8');
   }
 }
 
@@ -56,37 +151,68 @@ class ProductManager {
 ///////////////////////////////////////
 
 
-// Paso 1: Crear una instancia de la clase "ProductManager"
-const manager = new ProductManager();
+const path = 'src/db/productos.json'; // ruta de archivos
 
-// Paso 2: Llamar a "getProducts" recién creada la instancia, debe devolver un arreglo vacío []
-const productsEmpty = manager.getProducts();
-console.log(productsEmpty); // []
+// Paso 1: Crear una instancia para el path
+const productManager = new ProductManager(path);
 
-// Paso 3: Llamar al método "addProduct" con los campos proporcionados
-manager.addProduct({
-  title: "producto prueba",
-  description: "Este es un producto prueba",
+
+
+// Paso 2: Llamar a "getProducts"
+const products = productManager.getProducts();
+console.log(products); // debe devolver un array vacío []
+
+
+
+// Paso 3: Llamar "addProduct" con "producto prueba"
+const newProduct = {
+  title: 'producto prueba',
+  description: 'Este es un producto prueba',
   price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
   stock: 25
-});
+};
 
-// Paso 4: Verificar que el objeto se haya agregado correctamente con un id generado automáticamente SIN REPETIRSE
-const productsAdded = manager.getProducts();
-console.log(productsAdded);
+productManager.addProduct(newProduct);
 
-// Paso 5: Llamar al método "addProduct" con los mismos campos de arriba, debe arrojar un error porque el código estará repetido
-manager.addProduct({
-  title: "producto prueba",
-  description: "Este es un producto prueba",
-  price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
-  stock: 25
-});
 
-// Paso 6: Evaluar que "getProductById" devuelva error si no encuentra el producto o el producto en caso de encontrarlo
-const productNotFound = manager.getProductById(100); // Producto no encontrado, se mostrará un mensaje de error en la consola
-const productFound = manager.getProductById(1); // Producto encontrado, se mostrará el producto en la consola
+
+// Paso 4
+const updatedProducts = productManager.getProducts();
+console.log(updatedProducts); // [ { id: 1, title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail: 'Sin imagen', code: 'abc123', stock: 25 } ]
+
+
+
+// Paso 5
+const productId = 1;
+const productById = productManager.getProductById(productId);
+console.log(productById); // { id: 1, title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail: 'Sin imagen', code: 'abc123', stock: 25 }
+
+
+
+// Paso 6
+const updatedFields = { title: 'producto actualizado', price: 250 };
+productManager.updateProduct(productId, updatedFields);
+
+
+
+// Verificar que se haya actualizado correctamente
+const updatedProduct = productManager.getProductById(productId);
+console.log(updatedProduct); // { id: 1, title: 'producto actualizado', description: 'Este es un producto prueba', price: 250, thumbnail: 'Sin imagen', code: 'abc123', stock: 25 }
+
+
+
+// Paso 7
+productManager.deleteProduct(productId);
+
+
+
+// Verificar que se haya eliminado correctamente
+const deletedProduct = productManager.getProductById(productId);
+
+
+// Verificar que printee el error "producto no encontrado" correctamente
+console.log(deletedProduct);
+
+
